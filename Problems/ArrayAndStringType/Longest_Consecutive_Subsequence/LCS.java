0f5b5159ab5d1cb03be4025f9cedaf693b40bdbd
sorryma3er <whc0713air@gmail.com>
2025-11-22 23:34:06 +0700

    Added usage case to LCS.java.

--- Problems/ArrayAndStringType/Longest_Consecutive_Subsequence/LCS.java
+++ Problems/ArrayAndStringType/Longest_Consecutive_Subsequence/LCS.java
@@ -4,7 +4,26 @@ import java.util.*;
 
 public class LCS {
     public static void main(String[] args) {
-        
+        Solution sol = new Solution();
+
+        int[][] testCases = {
+            {},                               // empty
+            {100},                            // single element
+            {100, 4, 200, 1, 3, 2},           // classic example -> 4 (1,2,3,4)
+            {0, 3, 7, 2, 5, 8, 4, 6, 0, 1},   // -> 9 (0..8)
+            {9, 1, 4, 7, 3, -1, 0, 5, 8, -1, 6}, // -> 7 (-1..5 or 3..9 depending)
+            {10, 5, 12, 3, 55, 4, 11, 6},     // -> 4 (3,4,5,6)
+            {1, 2, 0, 1},                     // duplicates -> 3 (0,1,2)
+        };
+
+        int caseNo = 1;
+        for (int[] arr : testCases) {
+            int ans = sol.longestConsecutive(arr);
+            System.out.println("Test Case " + caseNo++);
+            System.out.println("  nums = " + Arrays.toString(arr));
+            System.out.println("  Longest consecutive length = " + ans);
+            System.out.println("-------------------------------------");
+        }
     }
 }
 
